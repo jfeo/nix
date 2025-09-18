35491bb2ef6bb98999e0bd2226ed5159bed351a5
Jens Feodor Nielsen <jens@feodor.dk>
2025-09-18 15:00:47 +0200

    Trying to get hyprland setup to work

--- home.nix
+++ home.nix
@@ -16,15 +16,19 @@
     username = "feo";
     homeDirectory = "/home/feo";
 
-    packages = [
-      pkgs.wget
-      pkgs.curl
+    packages = with pkgs; [
+      wget
+      curl
+      swww
+      hyprpolkitagent
+      dunst
+      wl-clipboard
     ];
 
-    file."${config.xdg.configHome}" = {
-      source = ./dotfiles;
-      recursive = true;
-    };
+    # file."${config.xdg.configHome}" = {
+    #   source = ./dotfiles;
+    #   recursive = true;
+    # };
 
     sessionVariables = {
       EDITOR = "vim";
@@ -33,9 +37,37 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    # set the flake package
+    # package defined in flake.nix
     package = null;
     portalPackage = null;
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    input.kb_layout = "dk";
+    bind = [
+      "$mod, Return, exec, alacritty"
+      "$mod, Q, killactive,"
+      "$mod, R, exec, rofi -show drun"
+      "$mod, F, exec, firefox"
+      ", Print, exec, grimblast copy area"
+    ]
+    ++ (
+      # workspaces
+      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+      builtins.concatLists (
+        builtins.genList (
+          i:
+          let
+            ws = i + 1;
+          in
+          [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        ) 9
+      )
+    );
   };
 
   programs.firefox.enable = true;
