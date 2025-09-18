1fa0cbdbc81b536654e94e2a23b9fe521c8fd7fd
Jens Feodor Nielsen <jens@feodor.dk>
2025-09-18 15:29:02 +0200

    Move hyprland config

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     inputs.nvf.homeManagerModules.default
+    ./hyprland.nix
   ];
 
   home = {
@@ -35,34 +36,34 @@
     };
   };
 
-  wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    input.kb_layout = "dk";
-    bind = [
-      "$mod, Return, exec, alacritty"
-      "$mod, Q, killactive,"
-      "$mod, R, exec, rofi -show drun"
-      "$mod, F, exec, firefox"
-      ", Print, exec, grimblast copy area"
-    ]
-    ++ (
-      # workspaces
-      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$mod, code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      )
-    );
-  };
+  # wayland.windowManager.hyprland.enable = true;
+  # wayland.windowManager.hyprland.settings = {
+  #   "$mod" = "SUPER";
+  #   input.kb_layout = "dk";
+  #   bind = [
+  #     "$mod, Return, exec, alacritty"
+  #     "$mod, Q, killactive,"
+  #     "$mod, R, exec, rofi -show drun"
+  #     "$mod, F, exec, firefox"
+  #     ", Print, exec, grimblast copy area"
+  #   ]
+  #   ++ (
+  #     # workspaces
+  #     # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+  #     builtins.concatLists (
+  #       builtins.genList (
+  #         i:
+  #         let
+  #           ws = i + 1;
+  #         in
+  #         [
+  #           "$mod, code:1${toString i}, workspace, ${toString ws}"
+  #           "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+  #         ]
+  #       ) 9
+  #     )
+  #   );
+  # };
 
   programs.firefox.enable = true;
 
