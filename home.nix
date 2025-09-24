47049f5fef4f9f77156b3007ebbb446bac11a618
Jens Feodor Nielsen <jens@feodor.dk>
2025-09-24 15:55:57 +0200

    Enable batsignal service to send battery-related notifications

--- home.nix
+++ home.nix
@@ -109,6 +109,18 @@
   services.swaync.enable = true;
   services.blueman-applet.enable = true;
   services.swayosd.enable = true;
+  services.batsignal.enable = true;
+  services.batsignal.extraArgs = [
+    "-w"
+    "20"
+    "-c"
+    "10"
+    "-d"
+    "5"
+    "-p"
+    "-m"
+    "2"
+  ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
