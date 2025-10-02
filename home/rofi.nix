f0cea2ef9d7b87782df3e0c9c32bad193118b312
Jens Feodor Nielsen <jens@feodor.dk>
2025-10-02 16:27:26 +0200

    rofi: theme adjustments

--- home/rofi.nix
+++ home/rofi.nix
@@ -1,4 +1,36 @@
-{ ... }:
+{ config, ... }:
+let
+  inherit (config.lib.formats.rasi) mkLiteral;
+in
 {
   programs.rofi.enable = true;
+
+  programs.rofi.theme = {
+    "@theme" = "catppuccin-default";
+    "@import" = "catppuccin-mocha";
+
+    configuration = {
+      show-icons = true;
+    };
+
+    window = {
+      border = mkLiteral "2px";
+      border-radius = mkLiteral "5px";
+      width = mkLiteral "40em";
+    };
+
+    message = {
+      border = mkLiteral "2px solid 0px 0px";
+    };
+
+    listview = {
+      border = mkLiteral "2px solid 0px 0px";
+    };
+
+    element = {
+      border-radius = mkLiteral "5px";
+      margin = mkLiteral "2px";
+      padding = mkLiteral "5px";
+    };
+  };
 }
