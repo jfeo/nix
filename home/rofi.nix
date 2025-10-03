eadcf48829ac6b23c84adf0bb075a5be34a6601b
Jens Feodor Nielsen <jens@feodor.dk>
2025-10-03 11:07:01 +0200

    rofi: spacing, icons, placeholder, rounding

--- home/rofi.nix
+++ home/rofi.nix
@@ -11,12 +11,17 @@ in
 
     configuration = {
       show-icons = true;
+      display-drun = "";
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
     };
 
     window = {
-      border = mkLiteral "2px";
-      border-radius = mkLiteral "5px";
+      border = mkLiteral "3px";
+      border-radius = mkLiteral "10px";
+      border-color = mkLiteral "@lavender";
       width = mkLiteral "40em";
+      padding = mkLiteral "5px";
     };
 
     message = {
@@ -31,6 +36,27 @@ in
       border-radius = mkLiteral "5px";
       margin = mkLiteral "2px";
       padding = mkLiteral "5px";
+    };
+
+    textbox-prompt-colon = {
+      str = "";
+    };
+
+    prompt = {
+      padding = mkLiteral "0 10px 0 0 ";
+    };
+
+    inputbar = {
+      padding = mkLiteral "5px";
+    };
+
+    entry = {
+      placeholder = "Run...";
+    };
+
+    scrollbar = {
+      handle-width = mkLiteral "5px";
+      handle-rounded-corners = true;
     };
   };
 }
