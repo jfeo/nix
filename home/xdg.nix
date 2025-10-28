a10aea3e95d33f0c901cdf7ef3cd76ecd0bf94e3
Jens Feodor Nielsen <jens@feodor.dk>
2025-10-28 08:54:29 +0100

    home: firefox as default browser

--- home/xdg.nix
+++ home/xdg.nix
@@ -9,6 +9,18 @@
       "image/png" = "qimgv.desktop";
 
       "application/pdf" = "org.kde.okular.desktop";
+
+      # default browser
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
     };
   };
 }
