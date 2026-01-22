c7e2319574cbb5a6437dcd52933668a06398546f
Jens Feodor Nielsen <jens@feodor.dk>
2026-01-22 16:55:58 +0100

    swayidle: refactor commands

--- home/sway/idle.nix
+++ home/sway/idle.nix
@@ -1,38 +1,43 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  loginctl = "${pkgs.systemd}/bin/loginctl";
+  swaymsg = "${pkgs.sway}/bin/swaymsg";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+in {
   services.swayidle = {
     enable = true;
     events = {
-      before-sleep = "${pkgs.systemd}/bin/loginctl lock-session";
-      after-resume = "${pkgs.sway}/bin/swaymsg 'output * dpms on'";
+      before-sleep = "${loginctl} lock-session";
+      after-resume = "${swaymsg} 'output * dpms on'";
     };
     timeouts = [
       # set monitor backlight to minimum
       {
         timeout = 150;
-        command = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
-        resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+        command = "${brightnessctl} -s set 10";
+        resumeCommand = "${brightnessctl} -r";
       }
       # turn off keyboard backlight
       {
         timeout = 150;
-        command = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0";
-        resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight";
+        command = "${brightnessctl} -sd rgb:kbd_backlight set 0";
+        resumeCommand = "${brightnessctl} -rd rgb:kbd_backlight";
       }
       # lock screen
       {
         timeout = 300;
-        command = "${pkgs.systemd}/bin/loginctl lock-session";
+        command = "${loginctl} lock-session";
       }
       # screen off
       {
         timeout = 330;
-        command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'";
-        resumeCommand = "${pkgs.sway}/bin/swaymsg 'output * dpms on' && ${pkgs.brightnessctl}/bin/brightnessctl -r";
+        command = "${swaymsg} 'output * dpms off'";
+        resumeCommand = "${swaymsg} 'output * dpms on' && ${brightnessctl} -r";
       }
       # suspend
       {
         timeout = 1800;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
+        command = "${systemctl} suspend";
       }
     ];
   };
